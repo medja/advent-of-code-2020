8d2c5c648f0557eba7b8b9ae80f445a56afa22bb
Metod Medja <metod@medja.net>
2021-12-05 13:50:42 +0100

    Day 06: Custom Customs (Part B)

--- src/challenge/day_06.rs
+++ src/challenge/day_06.rs
@@ -1,19 +1,37 @@
-pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
-    let result = input
-        .split(|line| line.is_empty())
-        .map(|line| Answers::new(line).count())
-        .sum::<usize>();
+use std::ops::{BitAnd, BitOr};
 
-    Ok(result)
+pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
+    Ok(sum_answer_counts(input, Answers::bitor))
 }
 
+pub fn part_b(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
+    Ok(sum_answer_counts(input, Answers::bitand))
+}
+
+fn sum_answer_counts<F>(input: &[&str], f: F) -> usize
+where
+    F: Fn(Answers, Answers) -> Answers + Copy,
+{
+    input
+        .split(|line| line.is_empty())
+        .map(|lines| {
+            lines
+                .iter()
+                .map(|line| Answers::new(line))
+                .reduce(f)
+                .unwrap_or_default()
+                .count()
+        })
+        .sum::<usize>()
+}
+
+#[derive(Default)]
 struct Answers(u32);
 
 impl Answers {
-    fn new(lines: &[&str]) -> Answers {
-        let value = lines
-            .iter()
-            .flat_map(|line| line.bytes())
+    fn new(line: &str) -> Answers {
+        let value = line
+            .bytes()
             .fold(0u32, |acc, byte| acc | 1 << (byte - b'a') as usize);
 
         Answers(value)
@@ -21,5 +39,21 @@ impl Answers {
 
     fn count(&self) -> usize {
         self.0.count_ones() as usize
+    }
+}
+
+impl BitOr for Answers {
+    type Output = Answers;
+
+    fn bitor(self, rhs: Self) -> Self::Output {
+        Answers(self.0 | rhs.0)
+    }
+}
+
+impl BitAnd for Answers {
+    type Output = Answers;
+
+    fn bitand(self, rhs: Self) -> Self::Output {
+        Answers(self.0 & rhs.0)
     }
 }
