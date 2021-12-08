4aa860bb1993c9b4bb35a03f28075acaf762b345
Metod Medja <metod@medja.net>
2021-12-08 16:08:11 +0100

    Support differences of 2

--- src/challenge/day_10.rs
+++ src/challenge/day_10.rs
@@ -1,12 +1,33 @@
 pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
-    // Assume all jolt differences are either 1 or 3
-    let max = input
-        .iter()
-        .map(|line| line.parse::<usize>().unwrap())
-        .max()
-        .unwrap();
+    let mut odds = 0usize;
+    let mut max = 0usize;
+
+    for line in input {
+        let value = line.parse::<usize>()?;
+
+        if value & 1 == 1 {
+            odds += 1;
+        }
+
+        if value > max {
+            max = value;
+        }
+    }
+
+    let evens = input.len() - odds + 1;
 
     let x = (max - input.len()) / 2;
-    let y = input.len() - x;
-    Ok((x + 1) * y)
+    // & !1 accounts for an additional difference of 1 caused by an odd number of inputs
+    let y = abs_diff(odds, evens) & !1;
+    let z = input.len() - x - y;
+
+    Ok((x + 1) * z)
+}
+
+fn abs_diff(x: usize, y: usize) -> usize {
+    if x > y {
+        x - y
+    } else {
+        y - x
+    }
 }
