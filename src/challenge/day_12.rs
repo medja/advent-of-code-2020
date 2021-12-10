0bc460f3a93a3d6f6f0a2715702556fc37d002ad
Metod Medja <metod@medja.net>
2021-12-10 08:14:41 +0100

    Day 12: Rain Risk (Part B)

--- src/challenge/day_12.rs
+++ src/challenge/day_12.rs
@@ -1,9 +1,8 @@
 use anyhow::anyhow;
-use std::ops::{AddAssign, SubAssign};
 
 pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
-    let mut east: isize = 0;
-    let mut north: isize = 0;
+    let mut east = 0isize;
+    let mut north = 0isize;
     let mut direction = Direction::East;
 
     for line in input {
@@ -14,8 +13,8 @@ pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
             Action::South => north -= amount,
             Action::West => east -= amount,
             Action::North => north += amount,
-            Action::Left => direction -= amount,
-            Action::Right => direction += amount,
+            Action::Left => direction = direction.rotate(360 - amount),
+            Action::Right => direction = direction.rotate(amount),
             Action::Forward => match direction {
                 Direction::East => east += amount,
                 Direction::South => north -= amount,
@@ -26,6 +25,52 @@ pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
     }
 
     Ok(east.abs() + north.abs())
+}
+
+pub fn part_b(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
+    let mut waypoint_east = 10isize;
+    let mut waypoint_north = 1isize;
+    let mut ship_east = 0isize;
+    let mut ship_north = 0isize;
+
+    for line in input {
+        let (action, amount) = Action::parse(line)?;
+
+        match action {
+            Action::East => waypoint_east += amount,
+            Action::South => waypoint_north -= amount,
+            Action::West => waypoint_east -= amount,
+            Action::North => waypoint_north += amount,
+            Action::Left => rotate_point(&mut waypoint_north, &mut waypoint_east, 360 - amount),
+            Action::Right => rotate_point(&mut waypoint_north, &mut waypoint_east, amount),
+            Action::Forward => {
+                ship_east += waypoint_east * amount;
+                ship_north += waypoint_north * amount;
+            }
+        }
+    }
+
+    Ok(ship_east.abs() + ship_north.abs())
+}
+
+fn rotate_point(north: &mut isize, east: &mut isize, amount: isize) {
+    match amount {
+        90 => {
+            let tmp = *north;
+            *north = -*east;
+            *east = tmp;
+        }
+        180 => {
+            *east = -*east;
+            *north = -*north;
+        }
+        270 => {
+            let tmp = *north;
+            *north = *east;
+            *east = -tmp;
+        }
+        _ => {}
+    }
 }
 
 enum Action {
@@ -63,27 +108,21 @@ enum Direction {
     North,
 }
 
-impl AddAssign<isize> for Direction {
-    fn add_assign(&mut self, rhs: isize) {
+impl Direction {
+    fn rotate(self, angle: isize) -> Direction {
         let direction = match self {
             Direction::East => 0,
-            Direction::South => 90,
-            Direction::West => 180,
-            Direction::North => 270,
+            Direction::South => 1,
+            Direction::West => 2,
+            Direction::North => 3,
         };
 
-        *self = match (direction + rhs) % 360 {
+        match (direction + (angle / 90)) % 4 {
             0 => Direction::East,
-            90 => Direction::South,
-            180 => Direction::West,
-            270 => Direction::North,
-            direction => panic!("Invalid direction {}", direction),
+            1 => Direction::South,
+            2 => Direction::West,
+            3 => Direction::North,
+            _ => unreachable!(),
         }
-    }
-}
-
-impl SubAssign<isize> for Direction {
-    fn sub_assign(&mut self, rhs: isize) {
-        self.add_assign(360 - rhs);
     }
 }
