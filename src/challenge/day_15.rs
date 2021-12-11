f416464239b839387dce6020c06e73b9d47b87ee
Metod Medja <metod@medja.net>
2021-12-11 10:58:47 +0100

    Day 15: Rambunctious Recitation (Part B)

--- src/challenge/day_15.rs
+++ src/challenge/day_15.rs
@@ -1,17 +1,25 @@
 use std::collections::HashMap;
 
 pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
-    let mut last = 0usize;
-    let mut numbers = HashMap::<usize, usize>::new();
+    play_until(input[0], 2020)
+}
 
-    for (i, number) in input[0].split(',').enumerate() {
+pub fn part_b(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
+    play_until(input[0], 30000000)
+}
+
+fn play_until(starting_numbers: &str, last_step: usize) -> anyhow::Result<u64> {
+    let mut last = 0u64;
+    let mut numbers = HashMap::new();
+
+    for (i, number) in starting_numbers.split(',').enumerate() {
         last = number.parse()?;
-        numbers.insert(last, i);
+        numbers.insert(last, i as u64);
     }
 
     numbers.remove(&last);
 
-    for i in numbers.len()..(2020 - 1) {
+    for i in (numbers.len() as u64)..(last_step as u64 - 1) {
         last = i - numbers.insert(last, i).unwrap_or(i);
     }
 
