d705a30134be1212722cd6e2e5a55b972c7f2e46
Metod Medja <metod@medja.net>
2021-12-11 11:14:54 +0100

    Replace HashMap with array in memory game

--- src/challenge/day_15.rs
+++ src/challenge/day_15.rs
@@ -1,26 +1,32 @@
-use std::collections::HashMap;
-
 pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
-    play_until(input[0], 2020)
+    play_until::<2020>(input[0])
 }
 
 pub fn part_b(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
-    play_until(input[0], 30000000)
+    play_until::<30000000>(input[0])
 }
 
-fn play_until(starting_numbers: &str, last_step: usize) -> anyhow::Result<u64> {
-    let mut last = 0u64;
-    let mut numbers = HashMap::new();
+fn play_until<const N: usize>(starting_numbers: &str) -> anyhow::Result<usize> {
+    let mut last = 0usize;
+    let mut numbers = [usize::MAX; N];
 
     for (i, number) in starting_numbers.split(',').enumerate() {
         last = number.parse()?;
-        numbers.insert(last, i as u64);
+        numbers[last] = i;
     }
 
-    numbers.remove(&last);
+    numbers[last] = usize::MAX;
 
-    for i in (numbers.len() as u64)..(last_step as u64 - 1) {
-        last = i - numbers.insert(last, i).unwrap_or(i);
+    for i in numbers.len()..(N - 1) {
+        let time = numbers[last];
+
+        if time == usize::MAX {
+            last = 0;
+        } else {
+            last = i - time;
+        }
+
+        numbers[last] = i;
     }
 
     Ok(last)
