d8ddd028bdaf107996fd091c15e14716785e4d4c
Metod Medja <metod@medja.net>
2022-12-05 23:46:24 +0100

    Day 18: Operation Order (Part B)

--- src/challenge/day_18.rs
+++ src/challenge/day_18.rs
@@ -1,46 +1,86 @@
 pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
-    let result = input
-        .iter()
-        .map(|line| evaluate(line).map(|result| result.0).unwrap_or(0))
-        .sum::<u64>();
-
-    Ok(result)
+    Ok(solve(input, false))
 }
 
-fn evaluate(mut input: &str) -> anyhow::Result<(u64, &str)> {
-    let mut lhs: u64;
-    (lhs, input) = take_number(input)?;
+pub fn part_b(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
+    Ok(solve(input, true))
+}
 
-    while !input.is_empty() {
-        let rhs: u64;
-        let operator = input.as_bytes()[0];
+fn solve(input: &[&str], prioritize_sum: bool) -> u64 {
+    input
+        .iter()
+        .map(|line| Calculator::evaluate(line, prioritize_sum).unwrap())
+        .sum::<u64>()
+}
 
-        if operator == b')' {
-            return Ok((lhs, input[1..].trim_start_matches(' ')));
-        }
+struct Calculator<'a> {
+    input: &'a str,
+    prioritize_sum: bool,
+}
 
-        (rhs, input) = take_number(&input[2..])?;
+impl Calculator<'_> {
+    fn evaluate(input: &str, prioritize_sum: bool) -> anyhow::Result<u64> {
+        let mut calculator = Calculator {
+            input,
+            prioritize_sum,
+        };
 
-        lhs = match operator {
-            b'+' => lhs + rhs,
-            b'*' => lhs * rhs,
-            _ => unreachable!(),
-        }
+        calculator.evaluate_expression()
     }
 
-    Ok((lhs, input))
-}
+    fn evaluate_expression(&mut self) -> anyhow::Result<u64> {
+        let mut lhs = self.evaluate_sum()?;
 
-fn take_number(input: &str) -> anyhow::Result<(u64, &str)> {
-    if let Some(stripped) = input.strip_prefix('(') {
-        evaluate(stripped)
-    } else {
-        match input.find(|char: char| !char.is_ascii_digit()) {
-            None => Ok((input.parse()?, "")),
-            Some(index) => Ok((
-                input[..index].parse()?,
-                input[index..].trim_start_matches(' '),
-            )),
+        while !self.input.is_empty() {
+            let operator = self.input.as_bytes()[0];
+
+            if operator == b')' {
+                self.input = self.input[1..].trim_start_matches(' ');
+                break;
+            }
+
+            self.input = &self.input[2..];
+            let rhs = self.evaluate_sum()?;
+
+            match operator {
+                b'+' => lhs += rhs,
+                b'*' => lhs *= rhs,
+                _ => unreachable!(),
+            }
         }
+
+        Ok(lhs)
+    }
+
+    fn evaluate_sum(&mut self) -> anyhow::Result<u64> {
+        let mut lhs = self.evaluate_number()?;
+
+        if !self.prioritize_sum {
+            return Ok(lhs);
+        }
+
+        while self.input.starts_with('+') {
+            self.input = &self.input[2..];
+            lhs += self.evaluate_number()?;
+        }
+
+        Ok(lhs)
+    }
+
+    fn evaluate_number(&mut self) -> anyhow::Result<u64> {
+        if let Some(input) = self.input.strip_prefix('(') {
+            self.input = input;
+            return self.evaluate_expression();
+        }
+
+        let index = self
+            .input
+            .find(|char: char| !char.is_ascii_digit())
+            .unwrap_or(self.input.len());
+
+        let value = &self.input[..index];
+        self.input = self.input[index..].trim_start_matches(' ');
+
+        Ok(value.parse()?)
     }
 }
