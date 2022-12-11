10161bf5a22bb49dab2d9c300f55e224ebc4efac
Metod Medja <metod@medja.net>
2022-12-11 07:34:15 +0100

    Day 19: Monster Messages (Part B)

--- src/challenge/day_19.rs
+++ src/challenge/day_19.rs
@@ -1,19 +1,56 @@
 use anyhow::Context;
 
 pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
-    let (rules, input) = parse_and_input(input)?;
-    Ok(input.iter().filter(|line| rules.is_match(line)).count())
+    let (rules, input) = parse_and_input(input, false)?;
+    Ok(input.iter().filter(|line| rules.test(line)).count())
+}
+
+pub fn part_b(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
+    let (rules, input) = parse_and_input(input, true)?;
+    Ok(input.iter().filter(|line| rules.test(line)).count())
+}
+
+fn parse_and_input<'a>(
+    input: &'a [&'a str],
+    with_loops: bool,
+) -> anyhow::Result<(Rules, &'a [&'a str])> {
+    let index = input
+        .iter()
+        .position(|line| line.is_empty())
+        .context("Input does not contain an empty line")?;
+
+    let rules = Rules::parse(&input[..index], with_loops)?;
+    Ok((rules, &input[index + 1..]))
 }
 
 struct Rules(Vec<Pattern>);
 
 impl Rules {
-    fn is_match(&self, input: &str) -> bool {
-        matches!(self.0[0].strip_pattern(input, &self.0), Some(remaining) if remaining.is_empty())
+    fn parse(input: &[&str], with_loops: bool) -> anyhow::Result<Self> {
+        let mut patterns = vec![Pattern::Literal('\0'); input.len()];
+
+        for line in input {
+            let (id, pattern) = Pattern::parse(line)?;
+            patterns[id] = pattern;
+        }
+
+        if with_loops {
+            patterns[8] = Pattern::Double(Combination::parse("42")?, Combination::parse("42 8")?);
+            patterns[11] = Pattern::Double(
+                Combination::parse("42 31")?,
+                Combination::parse("42 11 31")?,
+            );
+        }
+
+        Ok(Rules(patterns))
+    }
+
+    fn test(&self, input: &str) -> bool {
+        self.0[0].test(input, 0, &self.0).contains(input.len())
     }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 enum Pattern {
     Literal(char),
     Single(Combination),
@@ -21,84 +58,144 @@ enum Pattern {
 }
 
 impl Pattern {
-    fn strip_pattern<'a>(&self, input: &'a str, rules: &[Pattern]) -> Option<&'a str> {
+    fn parse(input: &str) -> anyhow::Result<(usize, Self)> {
+        let (id, input) = input
+            .split_once(':')
+            .with_context(|| format!("{} does not contain a :", input))?;
+
+        let id = id.parse()?;
+
+        let pattern = match input.split_once('|') {
+            None if input.as_bytes()[1] == b'"' => {
+                Pattern::Literal(input[2..].chars().next().unwrap())
+            }
+            None => Pattern::Single(Combination::parse(input)?),
+            Some((first, second)) => {
+                Pattern::Double(Combination::parse(first)?, Combination::parse(second)?)
+            }
+        };
+
+        Ok((id, pattern))
+    }
+
+    fn test(&self, input: &str, start: usize, rules: &[Pattern]) -> BitSet {
+        if start >= input.len() {
+            return BitSet::empty();
+        }
+
         match self {
-            Pattern::Literal(literal) => input.strip_prefix(*literal),
-            Pattern::Single(combination) => combination.strip_pattern(input, rules),
+            Pattern::Literal(literal) => {
+                if input[start..].starts_with(*literal) {
+                    BitSet::single(start + 1)
+                } else {
+                    BitSet::empty()
+                }
+            }
+            Pattern::Single(combination) => combination.test(input, start, rules),
             Pattern::Double(first, second) => first
-                .strip_pattern(input, rules)
-                .or_else(|| second.strip_pattern(input, rules)),
+                .test(input, start, rules)
+                .union(&second.test(input, start, rules)),
         }
     }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 struct Combination {
     length: usize,
     references: [usize; 3],
 }
 
 impl Combination {
-    fn strip_pattern<'a>(&self, input: &'a str, rules: &[Pattern]) -> Option<&'a str> {
-        self.references[..self.length]
-            .iter()
-            .try_fold(input, |input, reference| {
-                rules[*reference].strip_pattern(input, rules)
-            })
-    }
-}
+    fn parse(input: &str) -> anyhow::Result<Self> {
+        let mut i = 0;
+        let mut references = [0usize; 3];
 
-fn parse_and_input<'a>(input: &'a [&'a str]) -> anyhow::Result<(Rules, &'a [&'a str])> {
-    let index = input
-        .iter()
-        .position(|line| line.is_empty())
-        .context("Input does not contain an empty line")?;
-
-    Ok((parse_rules(&input[..index])?, &input[index + 1..]))
-}
-
-fn parse_rules(input: &[&str]) -> anyhow::Result<Rules> {
-    let mut patterns = vec![Pattern::Literal('\0'); input.len()];
-
-    for line in input {
-        let (id, pattern) = parse_pattern(line)?;
-        patterns[id] = pattern;
-    }
-
-    Ok(Rules(patterns))
-}
-
-fn parse_pattern(input: &str) -> anyhow::Result<(usize, Pattern)> {
-    let (id, input) = input
-        .split_once(':')
-        .with_context(|| format!("{} does not contain a :", input))?;
-
-    let id = id.parse()?;
-
-    let pattern = match input.split_once('|') {
-        None if input.as_bytes()[1] == b'"' => Pattern::Literal(input[2..].chars().next().unwrap()),
-        None => Pattern::Single(parse_combination(input)?),
-        Some((first, second)) => {
-            Pattern::Double(parse_combination(first)?, parse_combination(second)?)
+        for value in input.split_ascii_whitespace() {
+            references[i] = value.parse()?;
+            i += 1;
         }
-    };
 
-    Ok((id, pattern))
-}
+        let combination = Combination {
+            length: i,
+            references,
+        };
 
-fn parse_combination(input: &str) -> anyhow::Result<Combination> {
-    let mut i = 0;
-    let mut references = [0usize; 3];
-
-    for value in input.split_ascii_whitespace() {
-        references[i] = value.parse()?;
-        i += 1;
+        Ok(combination)
     }
 
-    let combination = Combination {
-        length: i,
-        references,
-    };
+    fn test(&self, input: &str, start: usize, rules: &[Pattern]) -> BitSet {
+        (0..self.length).fold(BitSet::single(start), |set, i| {
+            set.iter().fold(BitSet::empty(), |set, start| {
+                set.union(&rules[self.references[i]].test(input, start, rules))
+            })
+        })
+    }
+}
 
-    Ok(combination)
+struct BitSet(u64, u64);
+
+impl BitSet {
+    fn empty() -> Self {
+        BitSet(0, 0)
+    }
+
+    fn single(index: usize) -> Self {
+        BitSet::empty().insert(index)
+    }
+
+    fn insert(mut self, index: usize) -> Self {
+        if index > 64 {
+            self.1 |= 1 << (index - 64);
+        } else {
+            self.0 |= 1 << index;
+        }
+
+        self
+    }
+
+    fn union(mut self, other: &Self) -> Self {
+        self.0 |= other.0;
+        self.1 |= other.1;
+        self
+    }
+
+    fn contains(&self, index: usize) -> bool {
+        if index > 64 {
+            (self.1 & (1 << (index - 64))) != 0
+        } else {
+            (self.0 & (1 << index)) != 0
+        }
+    }
+
+    fn iter(&self) -> impl Iterator<Item = usize> + '_ {
+        BitSetIterator {
+            index: 0,
+            set: self,
+        }
+    }
+}
+
+struct BitSetIterator<'a> {
+    index: usize,
+    set: &'a BitSet,
+}
+
+impl Iterator for BitSetIterator<'_> {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut i = self.index;
+
+        while i < 128 {
+            if self.set.contains(i) {
+                self.index = i + 1;
+                return Some(i);
+            }
+
+            i += 1;
+        }
+
+        self.index = i;
+        None
+    }
 }
