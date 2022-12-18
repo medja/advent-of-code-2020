a2ee4302da292fb3019b0ad63378b97c35dc49cb
Metod Medja <metod@medja.net>
2022-12-18 16:31:55 +0100

    Day 22: Crab Combat (Part B)

--- src/challenge/day_22.rs
+++ src/challenge/day_22.rs
@@ -1,23 +1,67 @@
 use std::cmp::Ordering;
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
 
 pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
-    let (mut first, mut second) = parse_decks(input)?;
+    let (mut santa, mut crab) = parse_decks(input)?;
 
-    while !first.is_empty() && !second.is_empty() {
-        let first_card = first.draw_card();
-        let second_card = second.draw_card();
+    while !santa.is_empty() && !crab.is_empty() {
+        let santa_card = santa.draw_card();
+        let crab_card = crab.draw_card();
 
-        match first_card.cmp(&second_card) {
-            Ordering::Less => second.add_cards([second_card, first_card]),
-            Ordering::Greater => first.add_cards([first_card, second_card]),
+        match santa_card.cmp(&crab_card) {
+            Ordering::Less => crab.add_cards([crab_card, santa_card]),
+            Ordering::Greater => santa.add_cards([santa_card, crab_card]),
             Ordering::Equal => unreachable!(),
         }
     }
 
-    let mut winner = if first.is_empty() { second } else { first };
+    let mut winner = if santa.is_empty() { crab } else { santa };
 
     Ok(winner.score())
+}
+
+pub fn part_b(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
+    let (santa, crab) = parse_decks(input)?;
+    Ok(play(santa, crab).1.score())
+}
+
+fn play(mut santa: Deck, mut crab: Deck) -> (bool, Deck) {
+    let mut previous_decks = HashSet::<(Deck, Deck)>::new();
+
+    while !santa.is_empty() && !crab.is_empty() {
+        let key = (santa, crab);
+
+        if previous_decks.contains(&key) {
+            return (true, key.0);
+        }
+
+        previous_decks.insert(key.clone());
+        (santa, crab) = key; // move back the card decks
+
+        let santa_card = santa.draw_card();
+        let crab_card = crab.draw_card();
+
+        let recurse =
+            santa_card as usize <= santa.card_count() && crab_card as usize <= crab.card_count();
+
+        let santa_wins = if recurse {
+            play(santa.copy(santa_card), crab.copy(crab_card)).0
+        } else {
+            santa_card.cmp(&crab_card) == Ordering::Greater
+        };
+
+        if santa_wins {
+            santa.add_cards([santa_card, crab_card]);
+        } else {
+            crab.add_cards([crab_card, santa_card]);
+        }
+    }
+
+    if santa.is_empty() {
+        (false, crab)
+    } else {
+        (true, santa)
+    }
 }
 
 fn parse_decks(input: &[&str]) -> anyhow::Result<(Deck, Deck)> {
@@ -25,7 +69,7 @@ fn parse_decks(input: &[&str]) -> anyhow::Result<(Deck, Deck)> {
     Ok((Deck::new(&input[..index])?, Deck::new(&input[index + 1..])?))
 }
 
-#[derive(Default)]
+#[derive(Default, Clone, Eq, PartialEq, Hash)]
 struct Deck(VecDeque<u8>);
 
 impl Deck {
@@ -42,6 +86,10 @@ impl Deck {
         self.0.is_empty()
     }
 
+    fn card_count(&self) -> usize {
+        self.0.len()
+    }
+
     fn draw_card(&mut self) -> u8 {
         self.0.pop_front().unwrap()
     }
@@ -49,6 +97,10 @@ impl Deck {
     fn add_cards(&mut self, cards: [u8; 2]) {
         self.0.push_back(cards[0]);
         self.0.push_back(cards[1]);
+    }
+
+    fn copy(&self, count: u8) -> Self {
+        Deck(self.0.iter().take(count as usize).copied().collect())
     }
 
     fn score(&mut self) -> usize {
