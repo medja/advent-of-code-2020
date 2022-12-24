4c308c0d75d74c9a8746f67b8be259b07c8a789d
Metod Medja <metod@medja.net>
2022-12-24 13:54:53 +0100

    Rework Day 23 Part A

--- src/challenge/day_23.rs
+++ src/challenge/day_23.rs
@@ -1,41 +1,80 @@
 pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
-    let mut cups = [0u8; 9];
-    cups.copy_from_slice(input[0].as_bytes());
+    Ok(Cups::<9>::new(input[0]).simulate())
+}
 
-    let mut cups = cups.map(|char| char - b'1');
-    let mut next_cups = [0u8; 9];
+struct Cup {
+    value: u8,
+    next: u8,
+}
 
-    for _ in 0..100 {
-        let mut destination = (cups[0] + 8) % 9;
+struct Cups<const N: usize>([Cup; N]);
 
-        while cups[1..4].contains(&destination) {
-            destination = (destination + 8) % 9;
+impl<const N: usize> Cups<N> {
+    fn new(input: &str) -> Self {
+        let mut cups = std::array::from_fn(|i| Cup {
+            value: i as u8,
+            next: i as u8 + 1,
+        });
+
+        for (i, value) in input.bytes().enumerate() {
+            cups[i].value = value - b'1';
         }
 
-        let index = cups.iter().position(|cup| *cup == destination).unwrap();
+        cups.last_mut().unwrap().next = 0;
 
-        // copy cups before removed cups
-        next_cups[0..=index - 4].copy_from_slice(&cups[4..=index]);
-        // copy removed cups
-        next_cups[index - 3..index].copy_from_slice(&cups[1..4]);
-        // copy cups after removed cups
-        next_cups[index..cups.len() - 1].copy_from_slice(&cups[index + 1..]);
-        // copy the first cup
-        next_cups[cups.len() - 1] = cups[0];
-
-        cups = next_cups;
+        Cups(cups)
     }
 
-    let index = cups.iter().position(|cup| *cup == 0).unwrap();
-    let mut result = 0usize;
+    fn simulate(&mut self) -> usize {
+        let length = self.0.len() as u8;
+        let mut current = 0;
 
-    for cup in &cups[index + 1..] {
-        result = 10 * result + *cup as usize + 1;
+        for _ in 0..100 {
+            let mut removed = [self.0[current].next as usize, 0, 0];
+            removed[1] = self.0[removed[0]].next as usize;
+            removed[2] = self.0[removed[1]].next as usize;
+
+            self.0[current].next = self.0[removed[2]].next;
+
+            let mut destination = (self.0[current].value + length - 1) % length;
+
+            while removed
+                .iter()
+                .any(|index| self.0[*index].value == destination)
+            {
+                destination = (destination + length - 1) % length;
+            }
+
+            let destination = self
+                .0
+                .iter()
+                .position(|cup| cup.value == destination)
+                .unwrap();
+
+            self.0[removed[2]].next = self.0[destination].next;
+            self.0[destination].next = removed[0] as u8;
+
+            current = self.0[current].next as usize;
+        }
+
+        let mut index = self
+            .0
+            .iter()
+            .find(|cup| cup.value == 0)
+            .map(|cup| cup.next)
+            .unwrap() as usize;
+
+        let mut result = 0;
+
+        loop {
+            let cup = &self.0[index];
+
+            if cup.value == 0 {
+                break result;
+            }
+
+            index = cup.next as usize;
+            result = (result * 10) + cup.value as usize + 1;
+        }
     }
-
-    for cup in &cups[..index] {
-        result = 10 * result + *cup as usize + 1;
-    }
-
-    Ok(result)
 }
